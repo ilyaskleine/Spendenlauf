df75c22e9d3c95d9e1df1d4243892e81805706a8
ilyaskleine <ilyaskleine07@gmail.com>
2024-05-18 23:25:08 +0200

    Einrichtung der Läuferverwaltung und des Live-Dashboards

--- public/js/runners.js
+++ public/js/runners.js
@@ -1,19 +1,25 @@
-var classes = []
-
-fetch('/api/admin/jahrgaenge')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello from Vue.js!',
+        jahrgaenge: [],
+        selected: NaN
+    },
+    mounted: function() {
+        vue = this;
+        fetch('/api/admin/jahrgaenge')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.jahrgaenge = data.results;
+            this.selected = this.loop[0]
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    classes = data.results;
-    console.log(classes)
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-  
+});
